9b8fe6c382d0eccd7ab62538ba9e9e18f5e98c1d
crazedsanity <crazedsanity@cddb29c9-281a-0410-9cd8-f0d94d4212b7>
2006-12-06 18:07:37 +0000

    Changed include to use cs-content/cs_globalFunctions.php, and use that classes' method "conditional_header()".

--- trunk/sample_files/public_html/index.php
+++ trunk/sample_files/public_html/index.php
@@ -1,7 +1,8 @@
 <?php
 
-require("includes.php");
-conditional_header("/content/index.php");
+require(dirname(__FILE__) . "/../lib/externals/cs-content/cs_globalFunctions.php");
+$gf = new cs_globalFunctions;
+$gf->conditional_header("/content/index.php");
 exit;
 
 ?>
