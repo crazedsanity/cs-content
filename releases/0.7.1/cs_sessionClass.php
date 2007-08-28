065bcc8526240dba4291150c9e46c12fb441b0c0
crazedsanity <crazedsanity@cddb29c9-281a-0410-9cd8-f0d94d4212b7>
2007-08-28 19:39:15 +0000

    *** RELEASE 0.7.1 ***
    NOTE: 0.7 was never officially released due to some confusion in the code 
    regarding the version it was checking.  The VERSION file said 0.7, whilst some 
    of the code referenced 0.6... it was easier to just release with a maintenance 
    version.  :) 
    
    Summary of changes:::
     * fixes for non-existent files in cs_fileSystemClass{}
     * count remaining lines in an open file
     * remove unnecessary "externals" path reference in sample files
     * fix broken logo on sample templates
     * internal checks for version consistency
     * new PostgreSQL database connection library, cs_phpDB
     * minor cleaning additions for SQL strings

---
+++ releases/0.7.1/cs_sessionClass.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * FILE INFORMATION:
+ * $HeadURL$
+ * $Id$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $LastChangedRevision$
+ */
+class cs_session{
+
+	protected $db;
+	public $uid;
+	public $sid;
+	public $sid_check = 1;
+	
+	//---------------------------------------------------------------------------------------------
+	function __construct($createSession=1) {
+		if($createSession) {
+			//now actually create the session.
+			session_start();
+		}
+		
+		//check if there's a uid in the session already.
+		//TODO: need a setting somewhere that says what the name of this var should be,
+		//	instead of always forcing "uid".
+		$this->uid = 0;
+		if($_SESSION['uid']) {
+			$this->uid = $_SESSION['uid'];
+		}
+		
+		//grab what our session_id is...
+		$this->sid = session_id();
+		
+	}//end __construct()
+	//---------------------------------------------------------------------------------------------
+	
+	
+	
+	//---------------------------------------------------------------------------------------------
+	/**
+	 * Returns a version string.
+	 */
+	public function get_version($asArray=FALSE) {
+		return('0.7.1');
+	}//end get_version()
+	//---------------------------------------------------------------------------------------------
+
+
+}//end cs_session{}
+?>
