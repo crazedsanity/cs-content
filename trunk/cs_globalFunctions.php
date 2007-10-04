62fd8904662db94a2c58f33d0eee31238d8333bd
crazedsanity <crazedsanity@cddb29c9-281a-0410-9cd8-f0d94d4212b7>
2007-10-04 04:15:09 +0000

    Cleaning NULL integers & datetime strings.
    /cs_globalFunctions.php:
    	* cleanString():
    		-- integer/numeric set to textual null with no quoting if it is a 
    		literal null.
    		-- don't automatically quote datetime strings.

--- trunk/cs_globalFunctions.php
+++ trunk/cs_globalFunctions.php
@@ -413,7 +413,13 @@ class cs_globalFunctions extends cs_versionAbstract {
 			case "integer":
 			case "numeric":
 				//Remove everything that's not numeric.
-				$cleanThis = preg_replace("/[^0-9]/","",$cleanThis);
+				if(is_null($cleanThis)) {
+					$cleanThis = "NULL";
+					$sqlQuotes = 0;
+				}
+				else {
+					$cleanThis = preg_replace("/[^0-9]/","",$cleanThis);
+				}
 			break;
 			
 			case "decimal":
@@ -453,9 +459,6 @@ class cs_globalFunctions extends cs_versionAbstract {
 				
 			case "datetime":
 				$cleanThis=preg_replace("/[^A-Za-z0-9\/: \-\'\.]/","",$cleanThis);
-				if(!preg_match('/\'/', $cleanThis)) {
-					$cleanThis="'" . $cleanThis . "'";
-				}
 			break;
 				
 			case "all":
