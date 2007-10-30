339880ae17a2c25ebdb8390acad2deaf6e0828eb
crazedsanity <crazedsanity@cddb29c9-281a-0410-9cd8-f0d94d4212b7>
2007-10-30 04:26:25 +0000

    *** RELEASE 0.10.0 ***
    SUMMARY OF CHANGES:::
    	* cs_globalFunctions::string_from_array() returns NULL (instead of 0) by default.
    	* cs_phpDB uses transaction layers (protect code from multiple commits)
    	* cs_globalFunctions::array_as_option_list() (new method)
    	* contentSystem::finish() uses a try/catch block to catch exceptions, can 
    	pass exception to exception_handler(), if it is defined.
    	* cs_globalFunctions::string_from_array() "order/limit" style fixed.

---
+++ releases/0.10/cs_sessionClass.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * FILE INFORMATION:
+ * $HeadURL$
+ * $Id$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $LastChangedRevision$
+ */
+
+require_once(dirname(__FILE__) ."/cs_versionAbstract.class.php");
+
+class cs_session extends cs_versionAbstract {
+
+	protected $db;
+	public $uid;
+	public $sid;
+	public $sid_check = 1;
+	
+	//---------------------------------------------------------------------------------------------
+	function __construct($createSession=1) {
+		if($createSession) {
+			//now actually create the session.
+			session_start();
+		}
+		
+		//check if there's a uid in the session already.
+		//TODO: need a setting somewhere that says what the name of this var should be,
+		//	instead of always forcing "uid".
+		$this->uid = 0;
+		if($_SESSION['uid']) {
+			$this->uid = $_SESSION['uid'];
+		}
+		
+		//grab what our session_id is...
+		$this->sid = session_id();
+		
+	}//end __construct()
+	//---------------------------------------------------------------------------------------------
+	
+	
+	
+	//---------------------------------------------------------------------------------------------
+	public function is_authenticated() {
+		return(FALSE);
+	}//end is_authenticated()
+	//---------------------------------------------------------------------------------------------
+
+
+}//end cs_session{}
+?>
