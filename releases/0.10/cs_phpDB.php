f2b75ca571d11b78ac94c5a2192512d83c93102e
crazedsanity <crazedsanity@cddb29c9-281a-0410-9cd8-f0d94d4212b7>
2007-10-31 17:44:42 +0000

    *** RELEASE 0.10.2 ***
    SUMMARY OF CHANGES:::
    	* check for errors after commit (cs_phpDB)
    	* remove extra debug_print() statements (cs_phpDB)
    
    SVN COMMAND:::
    merge -r203:HEAD https://cs-content.svn.sourceforge.net/svnroot/cs-content/trunk

--- releases/0.10/cs_phpDB.php
+++ releases/0.10/cs_phpDB.php
@@ -813,7 +813,6 @@ class cs_phpDB extends cs_versionAbstract {
 			if($this->inTrans && is_null($this->transactionTree)) {
 				$transLevel = $this->get_transaction_level();
 				//transaction started without using beginTrans()...
-				$this->gfObj->debug_print($this->transactionTree); 
 				$this->transactionTree = array();
 				$this->gfObj->debug_print(__METHOD__ .": transaction already started, transStatus=(". $transStatus ."), transLevel=(". $transLevel .")");
 				$this->transactionTree[] = "Already started...";
@@ -845,10 +844,11 @@ class cs_phpDB extends cs_versionAbstract {
 				$retval = 1;
 			}
 			$this->exec("COMMIT");
-		}
-		else {
-			$this->gfObj->debug_print(__METHOD__ .": transLevel is (". $transLevel ."), not committing... " .
-				$this->gfObj->debug_print($this->transactionTree,0));
+			
+			//check to see if there was an error (deferred constraints are checked at commit time)
+			if(strlen($this->errorMsg())) {
+				$retval = 0;
+			}
 		}
 		$this->get_transaction_status();
 		return($retval);
@@ -1122,7 +1122,6 @@ class cs_phpDB extends cs_versionAbstract {
 	public function get_transaction_level() {
 		if(is_array($this->transactionTree)) {
 			$retval = count($this->transactionTree);
-			$this->gfObj->debug_print($this->transactionTree);
 		}
 		else {
 			$retval = 0;
