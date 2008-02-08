1380942eb95cc9ec99a62f769a444bf8474ee5a7
crazedsanity <crazedsanity@cddb29c9-281a-0410-9cd8-f0d94d4212b7>
2008-02-08 23:17:16 +0000

    Fix cs_fileSystemClass to chroot things properly.
    /cs_fileSystemClass.php:
    	* __construct():
    		-- call resolve_path_with_dots() on the root.
    	* filename2absolute():
    		-- use resolve_path_with_dots() and check_chroot() to ensure the filename 
    		is within the allowed root.
    		-- NOTE: the original preg_replace() and preg_match() were broken, 
    		causing the method to incorrectly throw an exception that the given file 
    		was outside of the allowed directory.
    	* resolve_path_with_dots() [NEW]:
    		-- fix paths that have dots (like "/x/y/z/../../test" to be "/x/y/z/test").
    	* check_chroot() [NEW]:
    		-- determine if the given path is within the root path.

--- trunk/cs_fileSystemClass.php
+++ trunk/cs_fileSystemClass.php
@@ -43,6 +43,7 @@ class cs_fileSystemClass extends cs_versionAbstract {
 		}
 		
 		$this->gf = new cs_globalFunctions();
+		$this->root = $this->resolve_path_with_dots($this->root);
 		
 		//set the CURRENT working directory... this should be a RELATIVE path to $this->root.
 		if(($cwd) AND (is_dir($rootDir .'/'. $cwd)) AND (!ereg($this->root, $cwd))) {
@@ -409,18 +410,22 @@ class cs_fileSystemClass extends cs_versionAbstract {
 	private function filename2absolute($filename) {
 		
 		clearstatcache();
+		
+		$filename = $this->resolve_path_with_dots($filename);
+		
 		//see if it starts with a "/"...
 		if(preg_match("/^\//", $filename)) {
-			//it's an absolute path... see if it's one we can use.
-			$myCwd = preg_replace('/\//', '\\\/', $this->realcwd);
-			if(preg_match('/^'. $myCwd .'/', $filename)) {
-				$retval = $filename;
-			}
-			else {
-				throw new exception(__METHOD__ .": path is outside the allowed directory: ". $filename);
-			}
+			$retval = $filename;
 		} else {
 			$retval=$this->realcwd .'/'. $filename;
+			$retval = $this->resolve_path_with_dots($retval);
+			#debug_print(__METHOD__ .": realcwd=(". $this->realcwd .")");
+			#$this->resolve_path_with_dots($retval);
+		}
+		
+		if(!$this->check_chroot($retval)) {
+			debug_print(func_get_args());
+			throw new exception(__METHOD__ .": file is outside of allowed directory (". $retval .")");
 		}
 		
 		return($retval);
@@ -747,6 +752,90 @@ class cs_fileSystemClass extends cs_versionAbstract {
 		
 		return($retval);
 	}//end go_to_line()
+	//========================================================================================
+	
+	
+	
+	//========================================================================================
+	/**
+	 * Fix a path that contains "../".
+	 * 
+	 * EXAMPLE: changes "/home/user/blah/blah/../../test" into "/home/user/test"
+	 */
+	public function resolve_path_with_dots($path) {
+		
+		while(preg_match('/\/\//', $path)) {
+			$path = preg_replace('/\/\//', '/', $path);
+		}
+		$retval = $path;
+		if(strlen($path) && preg_match('/\./', $path)) {
+			
+			$isAbsolute = FALSE;
+			if(preg_match('/^\//', $path)) {
+				$isAbsolute = TRUE;
+				$path = preg_replace('/^\//', '', $path);
+			}
+			$pieces = explode('/', $path);
+			
+			$finalPieces = array();
+			for($i=0; $i < count($pieces); $i++) {
+				$dirName = $pieces[$i];
+				if($dirName == '.') {
+					//do nothing; don't bother appending.
+				}
+				elseif($dirName == '..') {
+					$rippedIndex = array_pop($finalPieces);
+				}
+				else {
+					$finalPieces[] = $dirName;
+				}
+			}
+			
+			$retval = $this->gf->string_from_array($finalPieces, NULL, '/');
+			if($isAbsolute) {
+				$retval = '/'. $retval;
+			}
+		}
+		
+		return($retval);
+	}//end resolve_path_with_dots()
+	//========================================================================================
+	
+	
+	
+	//========================================================================================
+	private function check_chroot($path) {
+		$path = $this->resolve_path_with_dots($path);
+		
+		//now, let's go through the root directory structure, & make sure $path is within that.
+		$rootPieces = explode('/', $this->root);
+		$pathPieces = explode('/', $path);
+		
+		
+		if($rootPieces[0] == '') {
+			array_shift($rootPieces);
+		}
+		if($rootPieces[(count($rootPieces) -1)] == '') {
+			array_pop($rootPieces);
+		}
+		if($pathPieces[0] == '') {
+			array_shift($pathPieces);
+		}
+		
+		$retval = TRUE;
+		$tmp = '';
+		foreach($rootPieces as $index=>$dirName) {
+			$pathDir = $pathPieces[$index];
+			if($pathDir != $dirName) {
+				$retval = FALSE;
+				debug_print(__METHOD__ .": failed... tmp=(". $tmp ."), dirName=(". $dirName .")");
+				break;
+			}
+			$tmp = $this->gf->create_list($tmp, $dirName, '/');
+		}
+		
+		return($retval);
+	}//end check_chroot()
 	//========================================================================================
 	
 	
