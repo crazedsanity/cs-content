99ce6cdd69fda4aea02699a875cd08620479bdfe
crazedsanity <crazedsanity@cddb29c9-281a-0410-9cd8-f0d94d4212b7>
2009-05-22 17:28:18 +0000

    Added "curYear" template var, since it is used in an example template.

--- trunk/1.0/contentSystem.class.php
+++ trunk/1.0/contentSystem.class.php
@@ -149,6 +149,7 @@ class contentSystem extends cs_contentAbstract {
 		//setup some default template vars.
 		$this->templateObj->add_template_var('date', date('m-d-Y'));
 		$this->templateObj->add_template_var('time', date('H:i:s'));
+		$this->templateObj->add_template_var('curYear', date('Y'));
 		
 		$myUrl = '/';
 		if(strlen($this->section) && $this->section !== 0) {
