c1c333c53eed3286b3bbfa0ce0fb9e775d5bbe88
crazedsanity <crazedsanity@cddb29c9-281a-0410-9cd8-f0d94d4212b7>
2011-01-19 15:36:24 +0000

    Add extra line for finding abstract classes.

--- trunk/1.0/bin/generateHints.bash
+++ trunk/1.0/bin/generateHints.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
-grep "^class " * -R --exclude=*.svn* --exclude=*.tmp| grep ".php" | cut --delimiter=" " --fields 1,2 | sed "s/class //" | sed "s/:/\|/"> class.hints
+grep -i "^class " * -R --exclude=*.svn* --exclude=*.tmp| grep ".php" | cut --delimiter=" " --fields 1,2 | sed "s/class //" | sed "s/:/\|/"
+grep -i "^abstract class " * -R --exclude=*.svn* --exclude=*.tmp| grep ".php" | cut --delimiter=" " --fields 1,3 | sed "s/abstract //" | sed "s/:/\|/"
