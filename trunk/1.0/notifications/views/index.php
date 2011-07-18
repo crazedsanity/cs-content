1470f1c3fbe7546622685e624518730d53a7cf1c
jmarkwar <jmarkwar@cddb29c9-281a-0410-9cd8-f0d94d4212b7>
2011-07-18 20:08:04 +0000

    adding uber cool leet outlook type notifications and such

---
+++ trunk/1.0/notifications/views/index.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+        <link rel="stylesheet" type="text/css" href="../css/jquery-ui-1.7.3.custom.css" />
+        
+        <script type="text/javascript" language="JavaScript" src="../js/jquery-1.6.2.min.js"></script>
+        <script type="text/javascript" language="JavaScript" src="../js/jquery-ui-1.7.3.custom.min.js"></script>
+        <script type="text/javascript" language="JavaScript" src="../js/notifications.js"></script>
+ </head>
+ <body>
+ 	<div id="main" class="main" style="position:relative">
+ 		<a onclick="addNotification('Message From Above','Your Pants Are On Fire!')">Test Me!</a>
+ 		<a onclick="slideMessage('message')">Slide Me!</a>
+ 		<a onclick="moveRight()">Move Me!</a>		
+ 	</div>
+ 	<div id="notificationArea"></div>
+ </body>
+</html>
