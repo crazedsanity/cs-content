2afd03a349b7fe8ba2b125dcdaa0740f33fd2846
Dan Falconer <github@crazedsanity.com>
2013-09-30 17:24:59 -0500

    Changes to fix issues with versions

--- abstract/cs_content.abstract.class.php
+++ abstract/cs_content.abstract.class.php
@@ -1,31 +1,32 @@
 <?php
 /*
  * Created on Jan 29, 2009
- * 
- * FILE INFORMATION:
- * 
- * $HeadURL$
- * $Id$
- * $LastChangedDate$
- * $LastChangedBy$
- * $LastChangedRevision$
  */
 
-
-
-abstract class cs_contentAbstract extends cs_versionAbstract {
+require_once(dirname(__FILE__) .'/../cs_version.class.php');
+abstract class cs_contentAbstract extends cs_versionAbstract implements cs_versionInterface {
+	
+	static public $version;
 	
 	//-------------------------------------------------------------------------
     function __construct($makeGfObj=true) {
-		$this->set_version_file_location(dirname(__FILE__) . '/../VERSION');
-		$this->get_version();
-		$this->get_project();
 		
 		if($makeGfObj === true) {
 			//make a cs_globalFunctions{} object.
 			$this->gfObj = new cs_globalFunctions();
 		}
     }//end __construct()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public static function GetVersionObject() {
+		if(!is_object(self::$version)) {
+			self::$version = new cs_version(dirname(__FILE__) .'/../VERSION');
+		}
+		return(self::$version);
+	}//end GetVersionObject()
 	//-------------------------------------------------------------------------
 	
 	
